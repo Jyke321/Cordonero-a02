0f0cd2e0be4583f286ea7f801321a423a696684c
Jyke321 <jyke321@gmail.com>
2021-09-15 12:38:35 -0400

    ex16

---
+++ exercise16/src/main/java/exercise16/Solution16.java
@@ -0,0 +1,17 @@
+package exercise16;
+
+import java.util.Scanner;
+
+public class Solution16 {
+    public static void main(String[] args) {
+        //prompt user for their age
+        //compare the age to the legal driving age: 16
+        //display whether they can drive based off
+        //a comparison of their age and the legal driving age
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your age? ");
+        int age = Integer.parseInt(input.nextLine());
+        String legibility = (age>=16)?"":"not ";
+        System.out.println("You are " + legibility + "old enough to legally drive.\n");
+    }
+}
