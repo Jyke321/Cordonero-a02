1e1ddf175d0c598c34c5cc804d199cb458e773c0
Jyke321 <jyke321@gmail.com>
2021-09-15 17:50:12 -0400

    ex19

---
+++ exercise18/src/main/java/exercise18/Solution18.java
@@ -0,0 +1,34 @@
+package exercise18;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Solution18 {
+    public static void main(String[] args) {
+        //Prompt user for temperature type
+        //Prompt user for starting temperature
+        //convert to opposite temperature
+        Scanner input = new Scanner(System.in);
+        System.out.print("""
+                Press C to convert from Fahrenheit to Celsius.
+                Press F to convert from Celsius to Fahrenheit.
+                Your choice:\s""");
+        char type = input.nextLine().toCharArray()[0];
+        type = Character.toLowerCase(type);
+        String tempType = (type=='c')?"Fahrenheit":"Celsius";
+        String convType = (type=='f')?"Fahrenheit":"Celsius";
+        System.out.print("Please enter the temperature in " + tempType + ": ");
+        double temp = Double.parseDouble(input.nextLine());
+        double convTemp;
+        if (type=='c') {
+            //C = (F − 32) × 5 / 9
+            convTemp = (temp - 32) * 5 / 9;
+        } else { //F = (C × 9 / 5) + 32
+            convTemp = (temp * 9 / 5) + 32;
+        }
+        DecimalFormat df = new DecimalFormat();
+        df.setMinimumFractionDigits(0);
+        df.setMaximumFractionDigits(1);
+        System.out.println("The temperature in " + convType +" is "+ df.format(convTemp) +".");
+    }
+}
