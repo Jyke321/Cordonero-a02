cae146e1fad14538562f596ec2320b0c84922821
Jyke321 <jyke321@gmail.com>
2021-09-17 21:19:47 -0400

    ex20

--- exercise19/src/main/java/exercise19/Solution19.java
+++ exercise19/src/main/java/exercise19/Solution19.java
@@ -5,8 +5,40 @@ package exercise19;
  *  Copyright 2021 Jacob Cordonero
  */
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Solution19 {
     public static void main(String[] args) {
+        //prompt the user for weight and height
+        //calculate the BMI using the formula
+        final double BOT_RANGE = 18.5, TOP_RANGE = 25;
+        double weight = getInput("Enter your weight: ");
+        double height = getInput("Enter your height: ");
 
+        //bmi = (weight / (height × height)) * 703
+        double bmi = (weight / (height * height)) * 703;
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(1);
+        System.out.println("Your BMI is " + df.format(bmi) + ".");
+        if (bmi > BOT_RANGE && bmi < TOP_RANGE)
+            System.out.println("You are within the ideal weight range.");
+        else
+            System.out.println("You are overweight. You should see your doctor.");
+    }
+    public static double getInput(String prompt) {
+        Scanner input = new Scanner(System.in);
+        boolean inputError;
+        double retValue = 0;
+        do {
+            System.out.print(prompt);
+            try {
+                retValue = Double.parseDouble(input.nextLine());
+                inputError = false;
+            } catch (Exception e) {
+                inputError = true;
+            }
+        } while (inputError);
+        return retValue;
     }
 }
--- exercise20/src/main/java/exercise20/Solution20.java
+++ exercise20/src/main/java/exercise20/Solution20.java
@@ -5,8 +5,39 @@ package exercise20;
  *  Copyright 2021 Jacob Cordonero
  */
 
-public class Solution20 {
-    public static void main(String[] args) {
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
+public class Solution20 {
+    private static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        //prompt user for the order amount and the state
+        System.out.print("What is the order amount? ");
+        double total = Double.parseDouble(in.nextLine());
+        double tax = getTaxInformationFromUser();
+        DecimalFormat df = new DecimalFormat();
+        df.setMinimumFractionDigits(2);
+        df.setMaximumFractionDigits(2);
+        tax = tax*total;
+        if(tax>0)
+            System.out.println("The tax is $" + df.format(tax) + ".");
+        System.out.println("The total is $" + df.format(Math.round((total + tax)*100)/100.0) + ".");
+    }
+
+    private static double getTaxInformationFromUser() {
+        double retVal = 0;
+        System.out.print("What state do you live in? ");
+        String buffer = in.nextLine();
+        if (buffer.compareTo("Wisconsin")==0) {
+            retVal = 0.05;
+            System.out.print("What county do you live in? ");
+            buffer = in.nextLine();
+            if (buffer.compareTo("Eau Claire")==0)
+                retVal += .005;
+            else if (buffer.compareTo("Dunn")==0)
+                retVal += .004;
+        } else if (buffer.compareTo("Illinois")==0)
+            retVal += 0.08;
+        return retVal;
     }
 }
